4742c9849eeda3bfb83a9747d928fc340fac801f
zhangxianping <937097345@qq.com>
2026-03-08 17:10:48 +0800

    build: 更新依赖并引入 `versioningit` 以实现版本自动化管理。

--- src/_version.py
+++ src/_version.py
@@ -1,16 +1,15 @@
 def _get_version() -> str:
-    # we use lazy imports to avoid importing modules that are not
-    # used when the use of this function is patched out at build time
-    from importlib.resources import files  # noqa: PLC0415
-    from pathlib import Path  # noqa: PLC0415
 
-    import versioningit  # noqa: PLC0415
+    from importlib.resources import files
+    from pathlib import Path
 
-    module_path = files("qcodes")
+    import versioningit
+
+    module_path = files("pyinst")
     if isinstance(module_path, Path):
         return versioningit.get_version(project_dir=Path(module_path).parent.parent)
     else:
         return "0.0"
 
 
-__version__ = "0.54.4"
+__version__ = "0.0.3"
