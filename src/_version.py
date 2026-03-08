c7777d2467d5a3f94aabf5f02e3f2beb1d1034fc
zhangxianping <937097345@qq.com>
2026-03-08 17:10:35 +0800

    feat: 添加 Keysight N9020B 和 R&S FSWP 仪器驱动，重构项目为 `src/pyinst` 包结构并引入版本管理

---
+++ src/_version.py
@@ -0,0 +1,16 @@
+def _get_version() -> str:
+    # we use lazy imports to avoid importing modules that are not
+    # used when the use of this function is patched out at build time
+    from importlib.resources import files  # noqa: PLC0415
+    from pathlib import Path  # noqa: PLC0415
+
+    import versioningit  # noqa: PLC0415
+
+    module_path = files("qcodes")
+    if isinstance(module_path, Path):
+        return versioningit.get_version(project_dir=Path(module_path).parent.parent)
+    else:
+        return "0.0"
+
+
+__version__ = "0.54.4"
